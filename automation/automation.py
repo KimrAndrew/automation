e3493a744f9b91c1a4778e5fd67af8a587ffe0c4
KimrAndrew <andrewrk1997@gmail.com>
2021-12-25 14:54:57 -0800

    fixed bug in email regex, appears to be working as intended now

--- automation/automation.py
+++ automation/automation.py
@@ -1,6 +1,6 @@
 import re
 
-valid_email_pattern = r'(?:\w+.)?(?:w+.)?\w+@\w+.\w{1,3}'
+valid_email_pattern = r'(?:\w+\.)?(?:w+\.)?\w+@\w+\.\w{1,3}'
 
 def get_mixed_notes():
     with open('documents/mixed_notes.txt','r') as notes_file:
